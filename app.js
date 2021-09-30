2f5eab251a55d92050165f5b2f65df04109a6023
Dasha <trofimova.dashas@gmail.com>
2021-09-30 16:45:39 +0300

    fixed remarks

--- app.js
+++ app.js
@@ -1,35 +1,31 @@
 const fs = require('fs');
 const path = require("path");
 
-const pathUsersDir = path.join(__dirname,'users')
+const pathUsersDirFemale = path.join(__dirname,'users','female_users');
+const pathUsersDirMale = path.join(__dirname,'users','male_users');
 
-fs.readdirSync(pathUsersDir).forEach(directory => {
+const ChangeFileFolder = (pathUsersDir,gender,newPathUsersDir) => {
+    fs.readdir(pathUsersDir,(err, data) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        data.forEach(file => {
+            fs.readFile(path.join(pathUsersDir, file),(err, data) => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                const dataParse = JSON.parse(data);
+                if (dataParse.gender === gender) {
+                    fs.rename(path.join(pathUsersDir, file), path.join(newPathUsersDir, file), (err) => {
+                        console.log(err);
+                    })
+                }
+            });
+        });
+    });
+};
 
-    fs.readdirSync(path.join(pathUsersDir,directory)).forEach(file => {
-
-        fs.readFile(path.join(pathUsersDir, directory, file),(err, data) => {
-
-            if (err) {
-                console.log(err)
-                return;
-            }
-
-            let directoryName = directory.split('_')[0]
-
-            if (JSON.parse(data).gender !== directoryName) {
-
-                JSON.parse(data).gender === "male" ?
-
-                fs.rename(path.join(pathUsersDir, directory, file), path.join(pathUsersDir, 'male_users', file), (err) => {
-                    console.log(err)
-                }) :
-
-                fs.rename(path.join(pathUsersDir, directory, file), path.join(pathUsersDir, 'female_users', file), (err) => {
-                    console.log(err)
-                })
-            }
-
-       })
-   });
-
-})
+ChangeFileFolder(pathUsersDirFemale, "male", pathUsersDirMale);
+ChangeFileFolder(pathUsersDirMale,"female",pathUsersDirFemale);
