cc86926fd99d4aebf14d9f5c3dbcba50b256f3f4
Dasha <trofimova.dashas@gmail.com>
2021-09-30 17:26:32 +0300

    changed practice task

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 const fs = require('fs');
-const path = require("path");
 
-const mkdirPath = path.join(__dirname, 'files');
-const folders = ['menOlder20', 'menYounger20', 'womenOlder20', 'womenYounger20']
 const users = [
     { name: 'Olivia', gender: 'female', age: 22 },
     { name: 'Emma', gender: 'female', age: 18 },
@@ -14,33 +11,25 @@ const users = [
     { name: 'Jack', gender: 'male', age: 45 },
     { name: 'Din', gender: 'male', age: 36 },
     { name: 'Rick', gender: 'male', age: 19},
-]
+];
 
-folders.forEach(folder => {
-    fs.mkdir(path.join(mkdirPath, folder), {recursive: true}, (e) => {
-        console.log(e);
+const CreateUsers = () => {
+    users.forEach(item => {
+        const data = JSON.stringify(item);
+        const {name, gender, age} = item;
+        if (gender === 'male') {
+            age <= 20 ? fs.writeFile(`${__dirname}/files/menYounger20/${name}.json`, data, (err) => {
+                console.log(err);
+            }) : fs.writeFile(`${__dirname}/files/menOlder20/${name}.json`, data, (err) => {
+                console.log(err);
+            });
+        } else if (gender === 'female') {
+            age <= 20 ?  fs.writeFile(`${__dirname}/files/womenYounger20/${name}.json`, data, (err) => {
+                console.log(err);
+            }) : fs.writeFile(`${__dirname}/files/womenOlder20/${name}.json`, data, (err) => {
+                console.log(err);
+            });
+        }
     });
-})
-
-users.forEach(user => {
-    let data = JSON.stringify(user)
-    let {name, gender, age} = user
-
-    if (gender === 'male' && age <= 20) {
-        fs.writeFile(`${__dirname}/files/menYounger20/${name}.json`, data, (err) => {
-            console.log(err);
-        });
-    } else if (gender === 'male' && age >= 20) {
-        fs.writeFile(`${__dirname}/files/menOlder20/${name}.json`, data, (err) => {
-            console.log(err);
-        });
-    } else if (gender === 'female' && age <= 20) {
-        fs.writeFile(`${__dirname}/files/womenYounger20/${name}.json`, data, (err) => {
-            console.log(err);
-        });
-    } else if (gender === 'female' && age >= 20) {
-        fs.writeFile(`${__dirname}/files/womenOlder20/${name}.json`, data, (err) => {
-            console.log(err);
-        });
-    }
-})
+};
+CreateUsers();
