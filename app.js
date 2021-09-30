088fd6203dab083cc0053c22513d28151ee16a3c
Dasha <trofimova.dashas@gmail.com>
2021-09-30 18:44:01 +0300

    added enters

--- app.js
+++ app.js
@@ -4,19 +4,22 @@ const path = require("path");
 const pathUsersDirFemale = path.join(__dirname,'users','female_users');
 const pathUsersDirMale = path.join(__dirname,'users','male_users');
 
-const ChangeFileFolder = (pathUsersDir,gender,newPathUsersDir) => {
+const changeFileFolder = (pathUsersDir,gender,newPathUsersDir) => {
     fs.readdir(pathUsersDir,(err, data) => {
         if (err) {
             console.log(err);
             return;
         }
+
         data.forEach(file => {
             fs.readFile(path.join(pathUsersDir, file),(err, data) => {
                 if (err) {
                     console.log(err);
                     return;
                 }
+
                 const dataParse = JSON.parse(data);
+
                 if (dataParse.gender === gender) {
                     fs.rename(path.join(pathUsersDir, file), path.join(newPathUsersDir, file), (err) => {
                         console.log(err);
@@ -27,5 +30,5 @@ const ChangeFileFolder = (pathUsersDir,gender,newPathUsersDir) => {
     });
 };
 
-ChangeFileFolder(pathUsersDirFemale, "male", pathUsersDirMale);
-ChangeFileFolder(pathUsersDirMale,"female",pathUsersDirFemale);
+changeFileFolder(pathUsersDirFemale,"male", pathUsersDirMale);
+changeFileFolder(pathUsersDirMale,"female",pathUsersDirFemale);
