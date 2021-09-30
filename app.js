89bbcba086ae391a92c274e4d913ada0fdcf4a87
Dasha <trofimova.dashas@gmail.com>
2021-09-30 15:11:23 +0300

    practice task

--- app.js
+++ app.js
@@ -1,35 +1,46 @@
 const fs = require('fs');
 const path = require("path");
 
-const pathUsersDir = path.join(__dirname,'users')
+const mkdirPath = path.join(__dirname, 'files');
+const folders = ['menOlder20', 'menYounger20', 'womenOlder20', 'womenYounger20']
+const users = [
+    { name: 'Olivia', gender: 'female', age: 22 },
+    { name: 'Emma', gender: 'female', age: 18 },
+    { name: 'Charlotte', gender: 'female', age: 34 },
+    { name: 'Amelia', gender: 'female', age: 41 },
+    { name: 'Mia', gender: 'female', age: 11 },
+    { name: 'Jim', gender: 'male', age: 9 },
+    { name: 'John', gender: 'male', age: 28 },
+    { name: 'Jack', gender: 'male', age: 45 },
+    { name: 'Din', gender: 'male', age: 36 },
+    { name: 'Rick', gender: 'male', age: 19},
+]
 
-fs.readdirSync(pathUsersDir).forEach(directory => {
+folders.forEach(folder => {
+    fs.mkdir(path.join(mkdirPath, folder), {recursive: true}, (e) => {
+        console.log(e);
+    });
+})
 
-    fs.readdirSync(path.join(pathUsersDir,directory)).forEach(file => {
+users.forEach(user => {
+    let data = JSON.stringify(user)
+    let {name, gender, age} = user
 
-        fs.readFile(path.join(pathUsersDir, directory, file),(err, data) => {
-
-            if (err) {
-                console.log(err)
-                return;
-            }
-
-            let directoryName = directory.split('_')[0]
-
-            if (JSON.parse(data).gender !== directoryName) {
-
-                JSON.parse(data).gender === "male" ?
-
-                fs.rename(path.join(pathUsersDir, directory, file), path.join(pathUsersDir, 'male_users', file), (err) => {
-                    console.log(err)
-                }) :
-
-                fs.rename(path.join(pathUsersDir, directory, file), path.join(pathUsersDir, 'female_users', file), (err) => {
-                    console.log(err)
-                })
-            }
-
-       })
-   });
-
+    if (gender === 'male' && age <= 20) {
+        fs.writeFile(`${__dirname}/files/menYounger20/${name}.json`, data, (err) => {
+            console.log(err);
+        });
+    } else if (gender === 'male' && age >= 20) {
+        fs.writeFile(`${__dirname}/files/menOlder20/${name}.json`, data, (err) => {
+            console.log(err);
+        });
+    } else if (gender === 'female' && age <= 20) {
+        fs.writeFile(`${__dirname}/files/womenYounger20/${name}.json`, data, (err) => {
+            console.log(err);
+        });
+    } else if (gender === 'female' && age >= 20) {
+        fs.writeFile(`${__dirname}/files/womenOlder20/${name}.json`, data, (err) => {
+            console.log(err);
+        });
+    }
 })
